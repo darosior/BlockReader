d93a4f25cc43aa9fc18c39780cca71591c5d0dc4
antoine <antoine@localhost.localdomain>
2018-04-07 17:02:47 +0200

    Parsing the transactions. Not functional yet.

--- main.cpp
+++ main.cpp
@@ -68,13 +68,34 @@ void displayBytes(uint8_t bytes[], uint32_t size, bool be = false){
 		}
 	}
 }
+
+/**
+*@brief display all bytes the tab contains, but trying to convert them as character
+*@param bytes -> a tab of bytes
+*@param size -> nb of bytes
+*@param be -> if set to true, for big endians
+*/
+void displayAnsiBytes(uint8_t bytes[], uint32_t size, bool be = false){
+	if(!be){
+		for(uint32_t i = 0; i < size; i++){
+			cout<<hex<<bytes[i];
+		}
+	}
+	//If big endian
+	else{
+		for(int32_t i = size; i >= 0; i--){
+			cout<<hex<<bytes[i];
+		}
+	}
+}
 /**
 *@brief reads the varInt, following https://en.bitcoin.it/wiki/Protocol_documentation#Variable_length_integer
 *@param f an input stream, where to read
 *@return a 64 bit int containing the int to read (even if it was a 8, 16 or 32 bit
 */
 uint64_t varInt(ifstream &f){
-	uint64_t tmp, buff, finalInt;
+	uint8_t tmp;
+	uint64_t buff, finalInt;
 	f.read((char*)&tmp, 1); //The byte which give information about the length of int
 	if(tmp < 0xfd){
 		finalInt = tmp;
@@ -104,10 +125,18 @@ int main(int argc, char * argv[]){
 	const uint32_t nbBlocks = atoi(argv[2]);
 	uint32_t curBlock = 0;
 	string filename;
-	uint8_t *magicBytes, *nbBytes, *version, *prevHash, *merkle, *timestamp, *target, *nonce, *txVersion;
-	uint64_t txCount, txIn, txOut;
+	uint8_t *magicBytes, *nbBytes, *version, *prevHash, *merkle, *timestamp, *target, *nonce, *txVersion, *inHash, *inIndex, *inScript, *inSeq, *outValue, *outScript, *lockTime;
+	uint64_t txCount, txIn, txOut, inScriptLen, outScriptLen; //Variable length integer
 	while(curBlock < nbBlocks){
-		filename = dirname+"/blk0000"+to_string(curBlock)+".dat"; // Concatenation of the block name + its number
+		if(curBlock < 10)
+			filename = dirname+"/blk0000"+to_string(curBlock)+".dat"; // Concatenation of the block name + its number
+		else if(curBlock < 100)
+			filename = dirname+"/blk000"+to_string(curBlock)+".dat";
+		else if(curBlock < 1000)
+			filename = dirname+"/blk00"+to_string(curBlock)+".dat";
+		else if(curBlock < 10000)
+			filename = dirname+"/blk0"+to_string(curBlock)+".dat";
+
 		ifstream blockFile(filename, ios::in | ios::binary | ios::ate);
 		if(blockFile.is_open()){
 			magicBytes = new uint8_t[4];// The magic bytes are on 4 bytes..
@@ -131,8 +160,7 @@ int main(int argc, char * argv[]){
 			blockFile.read((char*)target, TARGET_SIZE);
 			blockFile.read((char*)nonce, NONCE_SIZE);
 			txCount = varInt(blockFile);
-			blockFile.read((char*)txVersion, TXVERSION_SIZE);
-			blockFile.close();
+			
 			
 			cout<<"                                       ----------"<<endl;
 			cout<<"                                     || BLOCK #"<<curBlock<<" ||"<<endl;
@@ -157,12 +185,60 @@ int main(int argc, char * argv[]){
 			cout<<dec<<"Timestamp : "<<asctime(localtime(&t)); //No need to endl after asctime
 			cout<<hex<<"Target : "<<addBytes(target, TARGET_SIZE)<<endl;
 			cout<<dec<<"Nonce : "<<addBytes(nonce, NONCE_SIZE, true)<<endl;
-			cout<<dec<<txCount<<endl;
+			cout<<dec<<"Number of transactions : "<<txCount<<endl;
 			cout<<endl<<"-----------------DATA-------------------"<<endl;
-			cout<<dec<<addBytes(txVersion, TXVERSION_SIZE, true)<<endl;
+			for(uint64_t i = 1; i <= txCount; i++){
+				blockFile.read((char*)txVersion, TXVERSION_SIZE);
+				cout<<dec<<"Transaction n° "<<i;
+				cout<<dec<<" (Version : "<<addBytes(txVersion, 4, true)<<")"<<endl;
+				txIn = varInt(blockFile);
+				for(uint64_t j = 1; j<= txIn; j++){
+					inHash = new uint8_t[32];
+					blockFile.read((char*)inHash, 32);
+					inIndex = new uint8_t[8];
+					blockFile.read((char*)inIndex, 8);
+					inScriptLen = varInt(blockFile);
+					inScript = new uint8_t[inScriptLen];
+					inSeq = new uint8_t[8];
+					blockFile.read((char*)inSeq, 8);
+					
+					cout<<dec<<"	Input n° "<<j<<endl;
+					cout<<hex<<"		Hash : "<<addBytes(inHash, 32, true)<<endl;
+					cout<<hex<<"		Index : "<<addBytes(inIndex, 8, true)<<endl;
+					cout<<dec<<"		Script : ";
+					displayAnsiBytes(inScript, inScriptLen, true);
+					cout<<endl;
+					cout<<hex<<"		Sequence :"<<addBytes(inSeq, 8, true)<<endl;
+					
+					delete[] inHash;
+					delete[] inIndex;
+					delete[] inScript;
+					delete[] inSeq;
+					
+				}
+				txOut = varInt(blockFile);
+				cout<<endl<<endl<<txOut<<endl;
+				for(uint64_t j = 1; j<= txOut; j++){
+					outValue = new uint8_t[8];
+					blockFile.read((char*)outValue, 8);
+					outScriptLen = varInt(blockFile);
+					outScript = new uint8_t[outScriptLen];
+					
+					cout<<dec<<"	Output n° "<<j<<endl;
+					cout<<dec<<"		Value : "<<addBytes(outValue, 8, true)<<" Satoshis"<<endl;
+					cout<<dec<<"		Script : ";
+					displayAnsiBytes(outScript, outScriptLen, true);
+					cout<<endl;
+					
+					delete[] outValue;
+					delete[] outScript;
+				}
+				
+			}		
 			
 			cout<<endl<<endl<<endl;
 			
+			blockFile.close();
 			
 			delete[] magicBytes;
 			delete[] nbBytes;

9f0fefe9851516d38afeeb219ef1a4937e98a4ad
darosior <darosior@gmail.com>
2018-05-23 23:58:19 +0200

    Reading varInt and checking blockLength

--- src/Chain.cpp
+++ src/Chain.cpp
@@ -46,6 +46,33 @@ void Chain::set_fileName(){
 		_filename = _dataDir+"blk"+std::to_string(_curFile)+".dat";
 }
 
+/**
+*@brief reads the varInt, following https://en.bitcoin.it/wiki/Protocol_documentation#Variable_length_integer
+*@param f an input stream, where to read
+*@return a 64 bit int containing the int to read (even if it was a 8, 16 or 32 bit
+*/
+uint64_t Chain::readVarInt(std::ifstream &f){
+	uint8_t tmp;
+	uint64_t buff, finalInt;
+	f.read((char*)&tmp, 1); //The byte which give information about the length of int
+	if(tmp < 0xfd){
+		finalInt = tmp;
+	}
+	else if(tmp == 0xfd){
+		f.read((char*)&buff, 2);
+		finalInt = buff;
+	}
+	else if(tmp == 0xfe){
+		f.read((char*)&buff, 4);
+		finalInt = buff;
+	}
+	else if(tmp == 0xff){
+		f.read((char*)&buff, 8);
+		finalInt = buff;
+	}
+	return finalInt;
+}
+
 void Chain::read(){
 	int curBlock = 0;
 	while(curBlock < _nbToRead){
@@ -59,12 +86,28 @@ void Chain::read(){
 				f.seekg(_curPos);
 				f.read((char*)& _blocks[curBlock].header.magicBytes, 4);
 				f.read((char*)& _blocks[curBlock].header.blockLength, 4);
+				_curPos = f.tellg(); // To compare length of block with length of remaining data, see below
 				f.read((char*)& _blocks[curBlock].header.version, 4);
 				f.read((char*)& _blocks[curBlock].header.prevHash, 32);
 				f.read((char*)& _blocks[curBlock].header.merkleRoot, 32);
 				f.read((char*)& _blocks[curBlock].header.timestamp, 4);
 				f.read((char*)& _blocks[curBlock].header.target, 4);
 				f.read((char*)& _blocks[curBlock].header.nonce, 4);
+				_blocks[curBlock].header.varInt = readVarInt(f);
+				
+				//We have here the header, so we have the blockLength value, so we know how many bytes are following up until
+				//the end of the block (from the reading of blockLength) (cf https://en.bitcoin.it/wiki/Block#Block_structure)
+				int headerEndPos = f.tellg();
+				f.seekg(f.end);
+				length = f.tellg() - _curPos;
+				if(length > _blocks[curBlock].header.blockLength){
+					//Okay, we can go on
+					f.seekg(headerEndPos);
+					
+				}
+				else{
+				
+				}
 			}
 			else{
 				//We need to take what we can..
--- src/Chain.h
+++ src/Chain.h
@@ -17,6 +17,17 @@ class Chain{
 	
 		Chain(std::string dataDir, int nbToRead, std::string output);
 		Chain(std::string dataDir, int nbToRead, bool debug); // Print results to console
+		
+		/**
+ 		*@brief reads the varInt, following https://en.bitcoin.it/wiki/Protocol_documentation#Variable_length_integer
+		*@param f an input stream, where to read
+		*@return a 64 bit int containing the int to read (even if it was a 8, 16 or 32 bit
+		*/
+		uint64_t Chain::readVarInt(std::ifstream &f);
+		
+		/**
+ 		*@brief reads the specified number of blocks, and stock their spec in struct attributes
+		*/
 		void read();
 	
 	private:
